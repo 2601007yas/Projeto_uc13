04a96adb1572116c68321f094a2628277a362e8d
Yasmin Leite de Araújo <yasmin.laraujo@senacsp.edu.br>
2025-05-28 22:07:23 -0300

    Minha primeira rota em nodejs

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("Hello World");
-hhtyjnuny
+const express = require('express');
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.write("Hello World");
+    res.end();
+});
+
+app.listen(8080);
+
