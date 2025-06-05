f3b46df511f5af2a33ee1872e3ea153f4b1339a0
Yasmin Leite de Araújo <yasmin.laraujo@senacsp.edu.br>
2025-06-04 22:11:23 -0300

    Extensão MySQL2 e conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql2');
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'senac',
+    port: 3306,
+    database: 'ecommerce_pc'
+});
+
+conexao.connect(function(erro){
+    if (erro) {
+        console.error('Erro ao conectar ao baanco de dados:', erro);
+        return;
+    }
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
 
 app.get("/", function(req, res){
-    res.write("Hello World");
+    res.write("Corinthians");
     res.end();
 });
 
 app.listen(8080);
+
+
 
